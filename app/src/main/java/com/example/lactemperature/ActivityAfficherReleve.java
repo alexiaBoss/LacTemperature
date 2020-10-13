d0f896e15df62d6918b5df5803e8872eb4a958ea
lucas <llelong@la-joliverie.com>
2020-10-13 09:51:54 +0200

    Partie 2 : Ajout des bouton dans ActivityAfficherReleve + Ajout de AfficheReleve

--- app/src/main/java/com/example/lactemperature/ActivityAfficherReleve.java
+++ app/src/main/java/com/example/lactemperature/ActivityAfficherReleve.java
@@ -2,11 +2,13 @@ package com.example.lactemperature;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -20,8 +22,36 @@ public class ActivityAfficherReleve extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_afficher_releve);
 
-        Button buttonAfficherReleveValider = findViewById(R.id.buttonSaisieTemperatureValider);
+        Button buttonAfficherReleveValider = findViewById(R.id.buttonAfficherReleveValider);
         Button buttonAfficherReleveAnnuler = findViewById(R.id.buttonAfficherReleveAnnuler);
+
+        //on place un écouteur dessus:
+        View.OnClickListener ecouteur1 = new View.OnClickListener() {
+            //on implémente la méthode onclick
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.buttonAfficherReleveValider:
+                        // enregistrer les données dans la base
+                        //on passer les infos dans l'autre interface
+                        Intent i = new Intent (ActivityAfficherReleve.this, AfficheReleve.class);
+                        //i.putExtra("EXTRA_CPT",);
+                        finish();
+                        Toast.makeText(getApplicationContext(), "Ouverture de l'affichage", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.buttonAfficherReleveAnnuler:
+                        finish();
+                        Toast.makeText(getApplicationContext(), "Annulation de l'affichage", Toast.LENGTH_LONG).show();
+                        break;
+
+
+                }
+
+            }
+        };
+        //on affecte l'écouteur aux boutons:
+        buttonAfficherReleveValider.setOnClickListener(ecouteur1);
+        buttonAfficherReleveAnnuler.setOnClickListener(ecouteur1);
 
         final Calendar myCalendar = Calendar.getInstance();
         final EditText edittext = findViewById(R.id.dateReleveAfficherReleve);
