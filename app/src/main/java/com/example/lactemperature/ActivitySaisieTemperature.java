a466cbff1e68532a3c0fa95ce8e981ee484887e9
Alexia <abossiere@la-joliverie.com>
2020-10-13 09:29:18 +0200

    Partie 2 : Création des fichiers d'affichage des saisies + fonctionnement des boutons

--- app/src/main/java/com/example/lactemperature/ActivitySaisieTemperature.java
+++ app/src/main/java/com/example/lactemperature/ActivitySaisieTemperature.java
@@ -2,11 +2,13 @@ package com.example.lactemperature;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -20,8 +22,38 @@ public class ActivitySaisieTemperature extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saisie_temperature);
 
+        // gestion des boutons
         Button buttonSaisieTemperatureValider = findViewById(R.id.buttonSaisieTemperatureValider);
         Button buttonSaisieTemperatureAnnuler = findViewById(R.id.buttonSaisieTemperatureAnnuler);
+
+
+        //on place un écouteur dessus:
+        View.OnClickListener ecouteur1 = new View.OnClickListener() {
+            //on implémente la méthode onclick
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.buttonSaisieTemperatureValider:
+                        // enregistrer les données dans la base
+                        //on passer les infos dans l'autre interface
+                        Intent i = new Intent (ActivitySaisieTemperature.this, AfficheSaisieTemperature.class);
+                        //i.putExtra("EXTRA_CPT",);
+                        finish();
+                        Toast.makeText(getApplicationContext(), "Enregistrement des données de la saisie", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.buttonSaisieTemperatureAnnuler:
+                        finish();
+                        Toast.makeText(getApplicationContext(), "Annulation de la saisie", Toast.LENGTH_LONG).show();
+                        break;
+
+
+                }
+
+            }
+        };
+        //on affecte l'écouteur aux boutons:
+        buttonSaisieTemperatureValider.setOnClickListener(ecouteur1);
+        buttonSaisieTemperatureAnnuler.setOnClickListener(ecouteur1);
 
 
         final  Calendar myCalendar = Calendar.getInstance();
