c7d69e5dbab4c44bfd203af50cf3cc409f9ba824
Alexia <abossiere@la-joliverie.com>
2021-04-27 16:34:01 +0200

    Ajout des boutons retour sur les interfaces

--- app/src/main/java/com/example/lactemperature/ActivityModifierLac.java
+++ app/src/main/java/com/example/lactemperature/ActivityModifierLac.java
@@ -1,7 +1,11 @@
 package com.example.lactemperature;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ActivityModifierLac  extends Activity {
 
@@ -9,5 +13,34 @@ public class ActivityModifierLac  extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modif_lac);
+
+        //on associe à un objet java de type Button, un widget repéré physiquement par son id:
+        Button buttonRetourMenuLac = findViewById(R.id.buttonRetourMenuLac);
+        Button buttonValiderMAJLac = (Button) findViewById(R.id.buttonValiderMAJLac);
+
+        //on place un écouteur dessus:
+        View.OnClickListener ecouteur1 = new View.OnClickListener() {
+            //on implémente la méthode onclick
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.buttonValiderMAJLac:
+                        Intent intent = new Intent(ActivityModifierLac.this, ActivityMAJLacs.class);
+                        startActivity(intent);
+                        Toast.makeText(getApplicationContext(), "Enregistrement des données de la saisie", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.buttonRetourMenuLac:
+                        finish();
+                        Toast.makeText(getApplicationContext(), "Retour", Toast.LENGTH_LONG).show();
+                        finish();
+                        break;
+
+                }
+
+            }
+        };
+        //on affecte l'écouteur aux boutons:
+        buttonRetourMenuLac.setOnClickListener(ecouteur1);
+        buttonValiderMAJLac.setOnClickListener(ecouteur1);
     }
 }
