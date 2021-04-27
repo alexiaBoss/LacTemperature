cf2551725f2a014625cc890342adfc46095b05dc
lucas <llelong@la-joliverie.com>
2021-04-27 16:59:45 +0200

    Ajout Lac

--- app/src/main/java/com/example/lactemperature/ActivityAddLac.java
+++ app/src/main/java/com/example/lactemperature/ActivityAddLac.java
@@ -2,6 +2,8 @@ package com.example.lactemperature;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityAddLac extends Activity {
 
@@ -10,7 +12,14 @@ public class ActivityAddLac extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_lac);
 
+        EditText textNomLac = findViewById(R.id.editTextNomLac);
+        String nomLac = textNomLac.getText().toString();
 
+        EditText textLatitude = findViewById(R.id.editTextLatitude);
+        String latitude = textLatitude.getText().toString();
+
+        EditText textLongitude = findViewById(R.id.editTextLongitude);
+        String longitude = textLongitude.getText().toString();
 
 
 
